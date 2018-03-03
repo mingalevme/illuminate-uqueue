9f846613de873caf44493f46afd6ea17e2f658d2
Mikhail Mingalev <mingalevme@gmail.com>
2018-03-04 00:15:17 +0300

    Added testing for Redis integration

--- tests/PackageTest.php
+++ tests/PackageTest.php
@@ -4,6 +4,7 @@ namespace Mingalevme\Tests\Illuminate\UQueue;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Redis;
 
 trait PackageTest
 {
@@ -25,5 +26,27 @@ trait PackageTest
         $this->assertNotSame($id1, $id3);
 
         $this->assertCount(2, DB::select('SELECT * FROM jobs'));
+    }
+
+    public function testRedis()
+    {
+        Queue::setDefaultDriver('redis');
+
+        Redis::command('DEL', ['queues:default']);
+
+        $id1 = Queue::push(new Job(['foo' => 'bar']));
+        $id2 = Queue::push(new Job(['foo' => 'bar']));
+
+        $this->assertNotNull($id1);
+        $this->assertNotNull($id2);
+        $this->assertSame($id1, $id2);
+
+        $this->assertCount(1, Redis::command('ZRANGE', ['queues:default', 0, -1]));
+
+        $id3 = Queue::push(new Job(['foo2' => 'bar2']));
+
+        $this->assertNotSame($id1, $id3);
+
+        $this->assertCount(2, Redis::command('ZRANGE', ['queues:default', 0, -1]));
     }
 }
