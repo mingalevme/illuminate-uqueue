6d6bbae6bdb4b03adf5f221a836b537a3eddccfa
Mingalev Mikhail <mingalevme@gmail.com>
2019-09-18 20:50:32 +0300

    Trying to fix Please remove or rename the Redis facade alias in your "app" configuration file in order to avoid collision with the PHP Redis extension. (1)

--- tests/LaravelTest.php
+++ tests/LaravelTest.php
@@ -3,7 +3,7 @@
 namespace Mingalevme\Tests\Illuminate\UQueue;
 
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Database\ConnectionResolverInterface;
+use Illuminate\Foundation\Application;
 use Illuminate\Redis\RedisServiceProvider;
 use Illuminate\Foundation\Testing\TestCase as LaravelTestCase;
 use Mingalevme\Illuminate\UQueue\LaravelUQueueServiceProvider;
@@ -13,18 +13,19 @@ class LaravelTest extends LaravelTestCase
     use PackageTest;
 
     /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
+     * @return Application
      */
     public function createApplication()
     {
         require_once __DIR__ . '/../vendor/laravel/laravel/app/Console/Kernel.php';
+        /** @var Application $app */
         $app = require __DIR__ . '/../vendor/laravel/laravel/bootstrap/app.php';
         $app->make(Kernel::class)->bootstrap();
+        $app->instance(\Redis::class, null);
+        $app->forgetInstance(\Redis::class);
+        $app->make('config')->set(['app.aliases.Redis' => null]);
         $app->register(RedisServiceProvider::class);
         $app->register(LaravelUQueueServiceProvider::class);
-        $app->alias('db', ConnectionResolverInterface::class);
         return $app;
     }
 }
